db9d4a3fc9c87076bd4b9475be844c7d7a1e06fc
Len Woodward <Len@ProjektGopher.com>
2022-02-20 14:49:25 -0800

    wip

--- resources/js/Pages/Landing/Index.vue
+++ resources/js/Pages/Landing/Index.vue
@@ -110,15 +110,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
             <div class="absolute inset-0">
-              <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100" alt="People working on laptops" />
-              <div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply" />
+              <img class="h-full w-full object-cover" src="/img/angry-casual-brunette-woman-using-smartphone-isolated-on-a-gray-background-SBI-302774441.jpg" alt="Woman looking at phone, disgusted." />
+              <div class="absolute inset-0 bg-gradient-to-r from-purple-800/25 to-indigo-700/25 mix-blend-multiply" />
             </div>
             <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
               <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
                 <span class="block text-white">Take control of your</span>
-                <span class="block text-indigo-200">customer support</span>
+                <span class="block text-indigo-200">Twitter DMs</span>
               </h1>
-              <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+              <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">TweetDick is a middleware that sits between you and your DMs to help deal with unwanted dick pics.</p>
               <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                 <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
                   <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"> Get started </a>
@@ -167,8 +167,8 @@
                   </span>
                 </div>
                 <div class="mt-6">
-                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Stay on top of customer support</h2>
-                  <p class="mt-4 text-lg text-gray-500">Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.</p>
+                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Automatic attachment scanning</h2>
+                  <p class="mt-4 text-lg text-gray-500">Every time you get sent an image in your DMs, we'll intercept it, and run it through our AI to look for unwanted content. If it passes, we'll transparently do nothing. It it fails, we'll give you tools to deal with it.</p>
                   <div class="mt-6">
                     <a href="#" class="inline-flex bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"> Get started </a>
                   </div>

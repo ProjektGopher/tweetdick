e8c01930df22cef55bff9f6a115c1106252c69b4
Len Woodward <Len@ProjektGopher.com>
2022-03-12 21:03:58 -0800

    refactor

--- app/Http/Controllers/Twitter/AuthController.php
+++ app/Http/Controllers/Twitter/AuthController.php
@@ -8,35 +8,34 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
-class AuthController extends Controller {
-    public function redirect() {
-      return Socialite::driver('twitter')->redirect();
+class AuthController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('twitter')->redirect();
     }
 
-    public function callback() {
-      $twitter_user = Socialite::driver('twitter')->user();
-  
-      $user = User::where('twitter_id', $twitter_user->id)->first();
-   
-      if ($user) {
-          $user->update([
-              'twitter_token' => $twitter_user->token,
-          ]);
-      } else {
-          $user = User::create([
-              'name' => $twitter_user->name,
-              'email' => $twitter_user->email,
-              'password' => Hash::make(Str::random(16)),
-              'twitter_id' => $twitter_user->id,
-              'twitter_nickname' => $twitter_user->nickname,
-              'twitter_avatar' => $twitter_user->avatar,
-              'twitter_token' => $twitter_user->token,
-          ]);
-      }
-   
-      auth()->login($user);
-   
-      return redirect('/dashboard');
+    public function callback()
+    {
+        $twitter_user = Socialite::driver('twitter')->user();
+
+        $user = User::updateOrCreate(
+            [
+                'twitter_id' => $twitter_user->id,
+            ],
+            [
+                'name' => $twitter_user->name,
+                'email' => $twitter_user->email,
+                'password' => Hash::make(Str::random(16)),
+                'twitter_nickname' => $twitter_user->nickname,
+                'twitter_avatar' => $twitter_user->avatar,
+                'twitter_token' => $twitter_user->token,
+            ]
+        );
+
+        auth()->login($user);
+
+        return redirect('/dashboard');
     }
 }
 

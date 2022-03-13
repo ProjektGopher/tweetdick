21c1e0478f5c238306a1adc712bd55eb8c0ef09d
Len Woodward <Len@ProjektGopher.com>
2022-03-12 22:49:54 -0800

    extract components

--- resources/js/Pages/Landing/Index.vue
+++ resources/js/Pages/Landing/Index.vue
@@ -104,31 +104,8 @@
     </header>
 
     <main>
-      <!-- Hero section -->
-      <div class="relative">
-        <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
-            <div class="absolute inset-0">
-              <img class="h-full w-full object-cover" :src="'/img/angry-casual-brunette-woman-using-smartphone-isolated-on-a-gray-background-SBI-302774441.jpg'" alt="Woman looking at phone, disgusted." />
-              <div class="absolute inset-0 bg-gradient-to-r from-purple-800/25 to-indigo-700/25 mix-blend-multiply" />
-            </div>
-            <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-              <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                <span class="block text-white">Take control of your</span>
-                <span class="block text-indigo-200">Twitter DMs</span>
-              </h1>
-              <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">TweetDick is a middleware that sits between you and your DMs to help deal with unwanted dick pics.</p>
-              <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-                <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                  <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"> Get started </a>
-                  <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"> Live demo </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
+      <Pricing />
 
       <!-- Logo Cloud -->
       <div class="bg-gray-100">
@@ -387,6 +364,8 @@ import {
   XIcon,
 } from '@heroicons/vue/outline'
 import { ChevronDownIcon } from '@heroicons/vue/solid'
+import Hero from './Hero.vue'
+import Pricing from './Pricing.vue'
 
 const solutions = [
   {
@@ -565,6 +544,8 @@ export default {
     MenuIcon,
     SparklesIcon,
     XIcon,
+    Hero,
+    Pricing,
   },
   setup() {
     return {

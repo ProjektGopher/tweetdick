3a176fc131012478107cae892bd22aab3daa9ac7
Len Woodward <Len@ProjektGopher.com>
2022-03-14 11:42:38 -0700

    extract component

--- resources/js/Pages/Landing/Index.vue
+++ resources/js/Pages/Landing/Index.vue
@@ -106,77 +106,7 @@
       <Hero />
       <Pricing />
       <LogoCloud />
-
-      <!-- Alternating Feature Sections -->
-      <div class="relative pt-16 pb-32 overflow-hidden">
-        <div aria-hidden="true" class="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100" />
-        <div class="relative">
-          <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-            <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-              <div>
-                <div>
-                  <span class="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
-                    <InboxIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                  </span>
-                </div>
-                <div class="mt-6">
-                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Automatic attachment scanning</h2>
-                  <p class="mt-4 text-lg text-gray-500">Every time you get sent an image in your DMs, we'll intercept it, and run it through our AI to look for unwanted content. If it passes, we'll transparently do nothing. It it fails, we'll give you tools to deal with it.</p>
-                  <div class="mt-6">
-                    <a href="#" class="inline-flex bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"> Get started </a>
-                  </div>
-                </div>
-              </div>
-              <div class="mt-8 border-t border-gray-200 pt-6">
-                <blockquote>
-                  <div>
-                    <p class="text-base text-gray-500">&ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;</p>
-                  </div>
-                  <footer class="mt-3">
-                    <div class="flex items-center space-x-3">
-                      <div class="flex-shrink-0">
-                        <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-                      </div>
-                      <div class="text-base font-medium text-gray-700">Marcia Hill, Digital Marketing Manager</div>
-                    </div>
-                  </footer>
-                </blockquote>
-              </div>
-            </div>
-            <div class="mt-12 sm:mt-16 lg:mt-0">
-              <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg" alt="Inbox user interface" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="mt-24">
-          <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-            <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
-              <div>
-                <div>
-                  <span class="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
-                    <SparklesIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                  </span>
-                </div>
-                <div class="mt-6">
-                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Better understand your customers</h2>
-                  <p class="mt-4 text-lg text-gray-500">Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.</p>
-                  <div class="mt-6">
-                    <a href="#" class="inline-flex bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"> Get started </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-              <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg" alt="Customer profile user interface" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <AlternatingFeature />
       <GradientFeature />
 
       <!-- Stats section -->
@@ -243,8 +173,9 @@ import Hero from './Hero.vue'
 import Pricing from './Pricing.vue'
 import LogoCloud from './LogoCloud.vue'
 import GradientFeature from './GradientFeature.vue'
-import LoginWithTwitter from '@/Components/LoginWithTwitter.vue'
+import AlternatingFeature from './AlternatingFeature.vue'
 import Footer from './Footer.vue'
+import LoginWithTwitter from '@/Components/LoginWithTwitter.vue'
 import { icons } from '@/Composables/useIcons'
 
 const solutions = [
@@ -294,6 +225,7 @@ export default {
     Pricing,
     LogoCloud,
     GradientFeature,
+    AlternatingFeature,
     Footer,
     LoginWithTwitter,
   },

a4fdb76de1ec5b19c0b062cb07538b206f9d616e
Len Woodward <Len@ProjektGopher.com>
2022-03-14 11:34:12 -0700

    fix bug

--- resources/js/Components/LoginWithTwitter.vue
+++ resources/js/Components/LoginWithTwitter.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    :href="route('twitter.auth.redirect')"
+    :href="href"
     class="
       ml-8
       whitespace-nowrap
@@ -22,17 +22,32 @@
       hover:from-purple-700 hover:to-indigo-700
     "
   >
-    Sign in with twitter
-    <component :is="icons.twitter.icon" class="h-6 w-6 ml-1 text-white" aria-hidden="true" />
+    <slot>
+      Sign in with twitter
+      <component
+        :is="icons.twitter.icon"
+        class="h-6 w-6 ml-1 text-white"
+        aria-hidden="true"
+      />
+    </slot>
   </a>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { icons } from "@/Composables/useIcons";
 
 export default defineComponent({
+  props: {
+    href: {
+      type: String,
+      default: route("twitter.auth.redirect"),
+    },
+  },
   setup() {
-    return {};
+    return {
+      icons,
+    };
   },
 });
 </script>

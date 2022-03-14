2beae2990b0838cef7e53060cade38a9f0760c00
Len Woodward <Len@ProjektGopher.com>
2022-03-14 09:54:52 -0700

    design and wording

--- resources/js/Pages/Landing/Index.vue
+++ resources/js/Pages/Landing/Index.vue
@@ -50,8 +50,17 @@
             <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Company </a>
           </PopoverGroup>
           <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
-            <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"> Sign up </a>
+            <!-- <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              Sign in
+            </a> -->
+            <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700">
+              Sign in with twitter
+              <component
+                :is="footerNavigation.social.filter((item) => item.name === 'Twitter')[0].icon"
+                class="h-6 w-6 ml-1 text-white"
+                aria-hidden="true"
+              />
+            </a>
           </div>
         </div>
 
